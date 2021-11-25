66f7d07396079e8bebbadb760cbef2af99f3c677
Durga Nepal <durgain.np@gmail.com>
2021-11-25 18:11:49 +0900

    Add redux to Product Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,16 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 
-import { productListReducer } from "./reducers/productReducers";
+import {
+  productDetailReducer,
+  productListReducer,
+} from "./reducers/productReducers";
 
 const initialState = {};
-const reducer = combineReducers({ productList: productListReducer });
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailReducer,
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
