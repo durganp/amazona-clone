840f418ee1cf3c9f6f67103a19bb8be3b5510845
Durga Nepal <durgain.np@gmail.com>
2021-11-30 18:11:33 +0900

    Create Sampal Users in MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from "bcryptjs";
 const data = {
+  users: [
+    {
+      name: "Durga",
+      email: "durga@gmail.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "Ram",
+      email: "ram@gmail.com",
+      password: bcrypt.hashSync("2468", 8),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       _id: "1",
